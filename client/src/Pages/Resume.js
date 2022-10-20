ed8146c82ec2d7451fd139bc2abb706328ed15e3
Ellen White <evwhite2@github.com>
2022-10-20 12:02:42 -0500

    updated add name in  manifest

--- client/src/Pages/Resume.js
+++ client/src/Pages/Resume.js
@@ -13,7 +13,6 @@ function Resume(){
                     <h1>Education</h1>
                     <Card
                     heading="Masters in Business Administration"
-<<<<<<< HEAD
                     subheading="Loyola University Chicago, 2022"
                     text="Concentration in Information Systems including SQL, DBMS, and Python"
                     />
@@ -21,20 +20,9 @@ function Resume(){
                     heading="Certificate in Business Data Analytics"
                     subheading="Loyola University Chicago, 2022"
                     text="Machine Learning, Business Intelligence, Analytics"
-=======
-                    text="Concentration in Information Systems"
-                    subheading="Loyola University Chicago, Expected Graduation Spring 2022"
-                    />
-                    
-                    <Card
-                    heading="Certificate in Business Data Analytics"
-                    subheading="Loyola University Chicago, May 2022"
-                    text="Oracle SQL, Machine Learning, SPSS Modeler, Python, Tableau"
->>>>>>> 24ee9219231f833ac6cb64e34edd9c4bc91ee7f0
                     />
                     <Card
                     heading="Certificate of Full Stack Web Development"
-<<<<<<< HEAD
                     subheading="Northwestern University, 2020"
                     text="JavaScript, Node.js, HTML/CSS, MongoDB, SQL,React"
                     />
@@ -43,23 +31,12 @@ function Resume(){
                     heading="BS Environmental Science and Sustainablity"
                     subheading="University of Illinois at Urbana-Champaign, 2016"
                     text="Major in Society and Environment"
-=======
-                    subheading="Northwestern University, March 2020"
-                    text="MERN Stack and Project Management"
-                    />
-
-                    <Card
-                    heading="Bachelors of Science"
-                    subheading="University of Illinois at Urbana-Champaign, May 2016"
-                    text="Major of Environmental Science and Sustainablity"
->>>>>>> 24ee9219231f833ac6cb64e34edd9c4bc91ee7f0
                     />
                 </div>
 
                 <div className="col-lg-6">
                     <h1>Professtional Experience</h1>
                     <ListCard
-<<<<<<< HEAD
                     heading="Software Developer"
                     subheading="FDM Group, Inc / July 2020-Current"
                     text="Working under contract for Bank of America since October 2020"
@@ -67,20 +44,6 @@ function Resume(){
                     bullet2="Served as the lead analyst and assisted in UAT for the development project of a new trade blotter application, assisting development team with gathering business requirements, research, and documentation"
                     bullet3="Wrote python modules in the Quartz Environment that would serve to extract and catalog e-trading data from several sources into Tableau dashboard for impact analysis reporting in the global e-trading divisions and monitor volumes, anomalies, and potentially erroneous transactions
                     rote several python modules in the Quartz Environment that would serve to migrate e-trading data from several sources into the Hadoop environment."
-=======
-                    heading="Business Analyst"
-                    subheading="Contractor for Bank of America / Feb 2021 - Present"
-                    bullet1="Lead analyst for development project to build day trading blotter for Toronto desk."
-                    bullet2="Works closely with applications owners and developers to ensure enterprise change managment standards are followed and accurately describe technical change processes."
-                    bullet3="Lead analyst for market data onboarding requested within the AMRS rates market data space."
-                    />
-                    <ListCard
-                    heading="Applications Programmer"
-                    subheading="Contractor for Bank of America / Oct 2020 - Feb 2021"
-                    bullet1="Wrote several python modules in the Quartz Environment that would serve to migrate e-trading data from several sources into the Hadoop environment."
-                    bullet2="Built Tableau dashboards for the purposes of e-traders to track their volumes, anomalies, and potentially erroneous transactions."
-                    bullet3="Extracted and cataloged all data sources used for regular reporting in the global e-trading divisions to be used for impact analysis when migrating reporting to BI software."
->>>>>>> 24ee9219231f833ac6cb64e34edd9c4bc91ee7f0
                     />
                     <ListCard
                     heading="IT Consultant"
